d5b87f21497dc001f175413844bd6ee6ece2b885
Alleh Keyroz <Alleh.keyroz@gmail.com>
2025-07-16 02:25:57 +0000

    ja que estouramos a cota desse projeto,troque as credenciais para a de o

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,13 +5,15 @@ import { getFirestore } from "firebase/firestore";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBmaWJ6mjgmPvEpHt_JKjNFEXjX822ykgk",
-  authDomain: "keyroz-vision.firebaseapp.com",
-  projectId: "keyroz-vision",
-  storageBucket: "keyroz-vision.appspot.com",
-  messagingSenderId: "89657404037",
-  appId: "1:89657404037:web:d1c6f1bd53037a0e5c57ec"
+  apiKey: "AIzaSyCv1iJp9KJFziCRVGiH45l420YZ4973eoo",
+  authDomain: "kdsvision-backup.firebaseapp.com",
+  projectId: "kdsvision-backup",
+  storageBucket: "kdsvision-backup.appspot.com",
+  messagingSenderId: "146882141427",
+  appId: "1:146882141427:web:ce60d74d286d1a9a036b86",
+  measurementId: "G-NH3EKCDYV6"
 };
 
 // Initialize Firebase
